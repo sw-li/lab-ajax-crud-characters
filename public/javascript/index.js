670de3a3d36286c08bbe0e68933e37d295d18e35
Shengwei LI <antoniovvtonyvv@gmail.com>
2023-01-18 14:49:12 +0100

    add new and edit need to be corrected

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -7,15 +7,16 @@ window.addEventListener('load', () => {
     charactersAPI.getAllCharacters()
     .then(results => { 
       const characters = results.data
+      charactersBlock.innerHTML =""
       for(let i=0; i < characters.length; i++){
-        charactersBlock.innerHTML =""
+        console.log(characters[i])
         charactersBlock.innerHTML += `      
         <div class="character-info">
-          <div class="name">Character Name: ${characters[i].name}</div>
-          <div class="id">Character Name: ${characters[i].id}</div>
-          <div class="occupation">Character Occupation: ${characters[i].occupation}</div>
-          <div class="cartoon">Is a Cartoon?: ${characters[i].cartoon}</div>
-          <div class="weapon">Character Weapon: ${characters[i].weapon}</div>
+          <div class="name">Name: ${characters[i].name}</div>
+          <div class="id">id: ${characters[i].id}</div>
+          <div class="occupation">Occupation: ${characters[i].occupation}</div>
+          <div class="cartoon">Cartoon?: ${characters[i].cartoon}</div>
+          <div class="weapon">Weapon: ${characters[i].weapon}</div>
         </div>`
       }
     })
@@ -23,32 +24,52 @@ window.addEventListener('load', () => {
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-    const characterID = document.getElementById("character-id").value
-    charactersAPI.getOneCharacter(characterID)
+    const fetchID = document.getElementById("character-id").value
+    charactersAPI.getOneCharacter(fetchID)
     .then(result =>  {
       //console.log(result.data)
       charactersBlock.innerHTML =""
       charactersBlock.innerHTML += `      
         <div class="character-info">
-          <div class="name">Character Name: ${result.data.name}</div>
-          <div class="id">Character Name: ${result.data.id}</div>
-          <div class="occupation">Character Occupation: ${result.data.occupation}</div>
-          <div class="cartoon">Is a Cartoon?: ${result.data.cartoon}</div>
-          <div class="weapon">Character Weapon: ${result.data.weapon}</div>
+          <div class="name">Name: ${result.data.name}</div>
+          <div class="id">id: ${result.data.id}</div>
+          <div class="occupation">Occupation: ${result.data.occupation}</div>
+          <div class="cartoon">Cartoon?: ${result.data.cartoon}</div>
+          <div class="weapon">Weapon: ${result.data.weapon}</div>
         </div>` 
     })
     .catch(err => console.log(err))
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    const deleteID = document.getElementById("character-id-delete").value
+    charactersAPI.deleteCharacter(deleteID)
+    .catch(err => console.log(err))
+  });
+
+  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    const newCharacter = {
+    name: document.getElementById("newName").value,
+    occupation: document.getElementById('newOccupation').value,
+    weapon: document.getElementById("newWeapon").value,
+    cartoon:document.getElementById("newCartoonBool").value
+  }
+    
+    charactersAPI.createCharacter(newCharacter)
+    .then(result => console.log(result.data))
+    .catch(err => console.log(err))
 
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    const editCharacterID = document.getElementById("editID").value
+    const editCharacter = {
+      name: document.getElementById("editName").value,
+      occupation: document.getElementById('editOccupation').value,
+      weapon: document.getElementById("editWeapon").value,
+      cartoon:document.getElementById("editCartoonBool").value}
+      charactersAPI.editCharacter(editCharacterID, editCharacter)
+      .catch(err => console.log(err))
+  })
 
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
 });
